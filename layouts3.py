2f9fb6660059c51173dd4524a2d7ea56a913401a
mihoku <49892705+mihoku@users.noreply.github.com>
2021-01-13 00:20:07 +0700

    Add files via upload

--- layouts3.py
+++ layouts3.py
@@ -61,13 +61,14 @@ def generate_form(i):
         ],className="three columns pretty_container", style={'background-color':sectorColor[i]})
 
 
-layouts3 = dcc.Tab(label='Prediksi',children=[html.Div([
-
-                        html.Div([ #start of total channeling and NPL row div
+layouts3 = dcc.Tab(label='Evaluasi Tarif IJP dan Sektor Terdampak',children=[html.Div([
+    
+        html.Div([#start of prediction result
+                 html.Div([ #row div of prediction result
                         html.Div([ #start of column div for total SME Credit channeling
                             html.H5("Total Penyaluran Kredit UMKM", style={'font-weight':'bold'}),
                             html.H4(id ="total_credit", style={'font-weight':'bold', 'font-size':'36px'}),
-                            ], className="pretty_container six columns",
+                            ], className="pretty_container five columns",
                             style={'text-align':'center','background-color':'#fff', 'border-top':'6px solid #fd7e14'}
                             ),#end of column div for total SME credit channeling
                         html.Div([ #start of column div for total NPL Projection
@@ -76,15 +77,17 @@ layouts3 = dcc.Tab(label='Prediksi',children=[html.Div([
                             html.P(id="total_NPL_val")
                             ], className="pretty_container six columns",
                             style={'text-align':'center','background-color':'#fff', 'border-top':'6px solid #ffc107'}
-                            )#end of column div for total NPL projection
-                        ], className="row flex-display"), #end of total channeling and NPL row div
-                    
-                    html.Div([ #start of budgeting row div
+                            ),#end of column div for total NPL projection
+                    ],className="row flex-display") #end of pred result row div
+                 ],className=" twelve columns"),#end of pred result var div
+        
+                html.Div([#start of evaluation
+                 html.Div([ #row div of evaluation
                         html.Div([ #start of column div for IJP tarif
                             html.H5("Tarif IJP Kredit UMKM", style={'font-weight':'bold'}),
                             html.H1(id ="IJP_tarif", style={'font-weight':'bold', 'font-size':'44px'}),
                             html.P(id="IJP_tarif_exp")
-                            ], className="pretty_container four columns",
+                            ], className="pretty_container three columns",
                             style={'text-align':'center','background-color':'#fff', 'border-top':'6px solid #007bff'}
                             ),#end of column div for IJP tarif
                         html.Div([ #start of column div for IJP budget
@@ -99,7 +102,8 @@ layouts3 = dcc.Tab(label='Prediksi',children=[html.Div([
                             ], className="pretty_container four columns",
                             style={'text-align':'center','background-color':'#fff', 'border-top':'6px solid #6f42c1'}
                             ),#end of column div for loss limit budget
-                        ], className="row flex-display"), #end of budgeting row div
+                        ],className="row flex-display") #end of evaluation row div
+                 ],className=" twelve columns"),#end of evaluation div
     
     html.Div([#start of macroeconomic vars
               html.H5("Indikator Makro Ekonomi", style={"font-weight":"bold"}),
@@ -148,7 +152,7 @@ layouts3 = dcc.Tab(label='Prediksi',children=[html.Div([
               html.Div(children=[generate_form(i) for i in np.arange(18) #row div of macro vars
                             ],className="row flex-display") #end of macro vars row div
               ],className=" twelve columns"),#end of credit channeling var div
-    ], className="row pretty_container")])
+    ], className="row")])
 
 @app.callback(
     [Output("sector_NPL_{}".format(i), "children") for i in np.arange(18)],
@@ -184,73 +188,105 @@ layouts3 = dcc.Tab(label='Prediksi',children=[html.Div([
 ])
 def predict_NPL(EconGrowth,Inflasi,Unemployment,birate,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r):
     
+    credit_channeling = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r]
+    
+    inputPopulate = { 'SektorEkonomi': econSector,
+                     'pandemicTF': [1]*18,
+                     'Inflasi': [Inflasi]*18,
+                      'EconGrowth':[EconGrowth]*18,
+                      'Unemployment':[Unemployment]*18,
+                      'valueChannel':credit_channeling,
+                      'biRate':[birate]*18
+        }
+    
+    percent_NPL_prediction = model_rf.predict(pd.DataFrame(inputPopulate))
     #prediction
-    pred1 = model_rf.predict([[econSector[0], 1, Inflasi, EconGrowth, Unemployment, a/1000000000, birate]])
-    pred2 = model_rf.predict([[econSector[1], 1, Inflasi, EconGrowth, Unemployment, b/1000000000, birate]])    
-    pred3 = model_rf.predict([[econSector[2], 1, Inflasi, EconGrowth, Unemployment, c/1000000000, birate]])
-    pred4 = model_rf.predict([[econSector[3], 1, Inflasi, EconGrowth, Unemployment, d/1000000000, birate]])
-    pred5 = model_rf.predict([[econSector[4], 1, Inflasi, EconGrowth, Unemployment, e/1000000000, birate]])
-    pred6 = model_rf.predict([[econSector[5], 1, Inflasi, EconGrowth, Unemployment, f/1000000000, birate]])
-    pred7 = model_rf.predict([[econSector[6], 1, Inflasi, EconGrowth, Unemployment, g/1000000000, birate]])
-    pred8 = model_rf.predict([[econSector[7], 1, Inflasi, EconGrowth, Unemployment, h/1000000000, birate]])
-    pred9 = model_rf.predict([[econSector[8], 1, Inflasi, EconGrowth, Unemployment, i/1000000000, birate]])
-    pred10 = model_rf.predict([[econSector[9], 1, Inflasi, EconGrowth, Unemployment, j/1000000000, birate]])
-    pred11 = model_rf.predict([[econSector[10], 1, Inflasi, EconGrowth, Unemployment, k/1000000000, birate]])
-    pred12 = model_rf.predict([[econSector[11], 1, Inflasi, EconGrowth, Unemployment, l/1000000000, birate]])
-    pred13 = model_rf.predict([[econSector[12], 1, Inflasi, EconGrowth, Unemployment, m/1000000000, birate]])
-    pred14 = model_rf.predict([[econSector[13], 1, Inflasi, EconGrowth, Unemployment, n/1000000000, birate]])
-    pred15 = model_rf.predict([[econSector[14], 1, Inflasi, EconGrowth, Unemployment, o/1000000000, birate]])
-    pred16 = model_rf.predict([[econSector[15], 1, Inflasi, EconGrowth, Unemployment, p/1000000000, birate]])
-    pred17 = model_rf.predict([[econSector[16], 1, Inflasi, EconGrowth, Unemployment, q/1000000000, birate]])
-    pred18 = model_rf.predict([[econSector[17], 1, Inflasi, EconGrowth, Unemployment, r/1000000000, birate]])    
+    # pred1 = model_rf.predict([[econSector[0], 1, Inflasi, EconGrowth, Unemployment, a/1000000000, birate]])
+    # pred2 = model_rf.predict([[econSector[1], 1, Inflasi, EconGrowth, Unemployment, b/1000000000, birate]])    
+    # pred3 = model_rf.predict([[econSector[2], 1, Inflasi, EconGrowth, Unemployment, c/1000000000, birate]])
+    # pred4 = model_rf.predict([[econSector[3], 1, Inflasi, EconGrowth, Unemployment, d/1000000000, birate]])
+    # pred5 = model_rf.predict([[econSector[4], 1, Inflasi, EconGrowth, Unemployment, e/1000000000, birate]])
+    # pred6 = model_rf.predict([[econSector[5], 1, Inflasi, EconGrowth, Unemployment, f/1000000000, birate]])
+    # pred7 = model_rf.predict([[econSector[6], 1, Inflasi, EconGrowth, Unemployment, g/1000000000, birate]])
+    # pred8 = model_rf.predict([[econSector[7], 1, Inflasi, EconGrowth, Unemployment, h/1000000000, birate]])
+    # pred9 = model_rf.predict([[econSector[8], 1, Inflasi, EconGrowth, Unemployment, i/1000000000, birate]])
+    # pred10 = model_rf.predict([[econSector[9], 1, Inflasi, EconGrowth, Unemployment, j/1000000000, birate]])
+    # pred11 = model_rf.predict([[econSector[10], 1, Inflasi, EconGrowth, Unemployment, k/1000000000, birate]])
+    # pred12 = model_rf.predict([[econSector[11], 1, Inflasi, EconGrowth, Unemployment, l/1000000000, birate]])
+    # pred13 = model_rf.predict([[econSector[12], 1, Inflasi, EconGrowth, Unemployment, m/1000000000, birate]])
+    # pred14 = model_rf.predict([[econSector[13], 1, Inflasi, EconGrowth, Unemployment, n/1000000000, birate]])
+    # pred15 = model_rf.predict([[econSector[14], 1, Inflasi, EconGrowth, Unemployment, o/1000000000, birate]])
+    # pred16 = model_rf.predict([[econSector[15], 1, Inflasi, EconGrowth, Unemployment, p/1000000000, birate]])
+    # pred17 = model_rf.predict([[econSector[16], 1, Inflasi, EconGrowth, Unemployment, q/1000000000, birate]])
+    # pred18 = model_rf.predict([[econSector[17], 1, Inflasi, EconGrowth, Unemployment, r/1000000000, birate]])    
     
     pre = "Proyeksi NPL Kredit UMKM untuk sektor ekonomi "
     
     #processing sectoral NPL percentage
-    preds1 = "{:,.2f} %".format(pred1[0]*100)
-    preds2 = "{:,.2f} %".format(pred2[0]*100)
-    preds3 = "{:,.2f} %".format(pred3[0]*100)
-    preds4 = "{:,.2f} %".format(pred4[0]*100)
-    preds5 = "{:,.2f} %".format(pred5[0]*100)
-    preds6 = "{:,.2f} %".format(pred6[0]*100)
-    preds7 = "{:,.2f} %".format(pred7[0]*100)
-    preds8 = "{:,.2f} %".format(pred8[0]*100)
-    preds9 = "{:,.2f} %".format(pred9[0]*100)
-    preds10 = "{:,.2f} %".format(pred10[0]*100)
-    preds11 = "{:,.2f} %".format(pred11[0]*100)
-    preds12 = "{:,.2f} %".format(pred12[0]*100)
-    preds13 = "{:,.2f} %".format(pred13[0]*100)
-    preds14 = "{:,.2f} %".format(pred14[0]*100)
-    preds15 = "{:,.2f} %".format(pred15[0]*100)
-    preds16 = "{:,.2f} %".format(pred16[0]*100)
-    preds17 = "{:,.2f} %".format(pred17[0]*100)
-    preds18 = "{:,.2f} %".format(pred18[0]*100)
+    preds = ["{:,.2f} %".format(percent_NPL_prediction[0]*100) for i in np.arange(18)]
+    # preds1 = "{:,.2f} %".format(prediction[0]*100)
+    # preds2 = "{:,.2f} %".format(prediction[1]*100)
+    # preds3 = "{:,.2f} %".format(prediction[2]*100)
+    # preds4 = "{:,.2f} %".format(prediction[3]*100)
+    # preds5 = "{:,.2f} %".format(prediction[4]*100)
+    # preds6 = "{:,.2f} %".format(prediction[5]*100)
+    # preds7 = "{:,.2f} %".format(prediction[6]*100)
+    # preds8 = "{:,.2f} %".format(prediction[7]*100)
+    # preds9 = "{:,.2f} %".format(prediction[8]*100)
+    # preds10 = "{:,.2f} %".format(prediction[9]*100)
+    # preds11 = "{:,.2f} %".format(prediction[10]*100)
+    # preds12 = "{:,.2f} %".format(prediction[11]*100)
+    # preds13 = "{:,.2f} %".format(prediction[12]*100)
+    # preds14 = "{:,.2f} %".format(prediction[13]*100)
+    # preds15 = "{:,.2f} %".format(prediction[14]*100)
+    # preds16 = "{:,.2f} %".format(prediction[15]*100)
+    # preds17 = "{:,.2f} %".format(prediction[16]*100)
+    # preds18 = "{:,.2f} %".format(prediction[17]*100)
+    # preds1 = "{:,.2f} %".format(pred1[0]*100)
+    # preds2 = "{:,.2f} %".format(pred2[0]*100)
+    # preds3 = "{:,.2f} %".format(pred3[0]*100)
+    # preds4 = "{:,.2f} %".format(pred4[0]*100)
+    # preds5 = "{:,.2f} %".format(pred5[0]*100)
+    # preds6 = "{:,.2f} %".format(pred6[0]*100)
+    # preds7 = "{:,.2f} %".format(pred7[0]*100)
+    # preds8 = "{:,.2f} %".format(pred8[0]*100)
+    # preds9 = "{:,.2f} %".format(pred9[0]*100)
+    # preds10 = "{:,.2f} %".format(pred10[0]*100)
+    # preds11 = "{:,.2f} %".format(pred11[0]*100)
+    # preds12 = "{:,.2f} %".format(pred12[0]*100)
+    # preds13 = "{:,.2f} %".format(pred13[0]*100)
+    # preds14 = "{:,.2f} %".format(pred14[0]*100)
+    # preds15 = "{:,.2f} %".format(pred15[0]*100)
+    # preds16 = "{:,.2f} %".format(pred16[0]*100)
+    # preds17 = "{:,.2f} %".format(pred17[0]*100)
+    # preds18 = "{:,.2f} %".format(pred18[0]*100)
     
     #processing NPL sectoral value
-    pref1 = pre+econSector[0]+" {:,.2f}".format(pred1[0]*a)
-    pref2 = pre+econSector[1]+" {:,.2f}".format(pred2[0]*b)
-    pref3 = pre+econSector[2]+" {:,.2f}".format(pred3[0]*c)
-    pref4 = pre+econSector[3]+" {:,.2f}".format(pred4[0]*d)
-    pref5 = pre+econSector[4]+" {:,.2f}".format(pred5[0]*e)
-    pref6 = pre+econSector[5]+" {:,.2f}".format(pred6[0]*f)
-    pref7 = pre+econSector[6]+" {:,.2f}".format(pred7[0]*g)
-    pref8 = pre+econSector[7]+" {:,.2f}".format(pred8[0]*h)
-    pref9 = pre+econSector[8]+" {:,.2f}".format(pred9[0]*i)
-    pref10 = pre+econSector[9]+" {:,.2f}".format(pred10[0]*j)
-    pref11 = pre+econSector[10]+" {:,.2f}".format(pred11[0]*k)
-    pref12 = pre+econSector[11]+" {:,.2f}".format(pred12[0]*l)
-    pref13 = pre+econSector[12]+" {:,.2f}".format(pred13[0]*m)
-    pref14 = pre+econSector[13]+" {:,.2f}".format(pred14[0]*n)
-    pref15 = pre+econSector[14]+" {:,.2f}".format(pred15[0]*o)
-    pref16 = pre+econSector[15]+" {:,.2f}".format(pred16[0]*p)
-    pref17 = pre+econSector[16]+" {:,.2f}".format(pred17[0]*q)
-    pref18 = pre+econSector[17]+" {:,.2f}".format(pred18[0]*r)
+    pref = [pre+econSector[i]+" {:,.2f}".format(percent_NPL_prediction[i]*credit_channeling[i]) for i in np.arange(18)]
+    # pref1 = pre+econSector[0]+" {:,.2f}".format(pred1[0]*a)
+    # pref2 = pre+econSector[1]+" {:,.2f}".format(pred2[0]*b)
+    # pref3 = pre+econSector[2]+" {:,.2f}".format(pred3[0]*c)
+    # pref4 = pre+econSector[3]+" {:,.2f}".format(pred4[0]*d)
+    # pref5 = pre+econSector[4]+" {:,.2f}".format(pred5[0]*e)
+    # pref6 = pre+econSector[5]+" {:,.2f}".format(pred6[0]*f)
+    # pref7 = pre+econSector[6]+" {:,.2f}".format(pred7[0]*g)
+    # pref8 = pre+econSector[7]+" {:,.2f}".format(pred8[0]*h)
+    # pref9 = pre+econSector[8]+" {:,.2f}".format(pred9[0]*i)
+    # pref10 = pre+econSector[9]+" {:,.2f}".format(pred10[0]*j)
+    # pref11 = pre+econSector[10]+" {:,.2f}".format(pred11[0]*k)
+    # pref12 = pre+econSector[11]+" {:,.2f}".format(pred12[0]*l)
+    # pref13 = pre+econSector[12]+" {:,.2f}".format(pred13[0]*m)
+    # pref14 = pre+econSector[13]+" {:,.2f}".format(pred14[0]*n)
+    # pref15 = pre+econSector[14]+" {:,.2f}".format(pred15[0]*o)
+    # pref16 = pre+econSector[15]+" {:,.2f}".format(pred16[0]*p)
+    # pref17 = pre+econSector[16]+" {:,.2f}".format(pred17[0]*q)
+    # pref18 = pre+econSector[17]+" {:,.2f}".format(pred18[0]*r)
     
     #populate variables
-    credit_channeling = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r]
-    percent_NPL_prediction = [pred1[0],pred2[0],pred3[0],pred4[0],pred5[0],pred6[0],
-                           pred7[0],pred8[0],pred9[0],pred10[0],pred11[0],pred12[0],
-                           pred13[0],pred14[0],pred15[0],pred16[0],pred17[0],pred18[0]]
+    # credit_channeling = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r]
+    # percent_NPL_prediction = [pred1[0],pred2[0],pred3[0],pred4[0],pred5[0],pred6[0],
+    #                        pred7[0],pred8[0],pred9[0],pred10[0],pred11[0],pred12[0],
+    #                        pred13[0],pred14[0],pred15[0],pred16[0],pred17[0],pred18[0]]
     
     #processing total NPL percentage and value
     total_SME_credit_channeling = sum(credit_channeling)
@@ -276,4 +312,5 @@ def predict_NPL(EconGrowth,Inflasi,Unemployment,birate,a,b,c,d,e,f,g,h,i,j,k,l,m
     ijp_budget = "Rp {:,.2f}".format(ijp)
     loss_limit_budget = "Rp {:,.2f}".format(loss_lim)
     
-    return preds1, preds2, preds3, preds4, preds5, preds6, preds7, preds8, preds9, preds10, preds11, preds12, preds13, preds14, preds15, preds16, preds17, preds18, pref1, pref2, pref3, pref4, pref5, pref6, pref7, pref8, pref9, pref10, pref11, pref12, pref13, pref14, pref15, pref16, pref17, pref18, total_NPL_percentage_pass, total_NPL_val_pass, total_credit, ijp_tarif, ijp_budget, loss_limit_budget
+#    return preds1, preds2, preds3, preds4, preds5, preds6, preds7, preds8, preds9, preds10, preds11, preds12, preds13, preds14, preds15, preds16, preds17, preds18, pref1, pref2, pref3, pref4, pref5, pref6, pref7, pref8, pref9, pref10, pref11, pref12, pref13, pref14, pref15, pref16, pref17, pref18, total_NPL_percentage_pass, total_NPL_val_pass, total_credit, ijp_tarif, ijp_budget, loss_limit_budget
+    return preds[0], preds[1], preds[2], preds[3], preds[4], preds[5], preds[6], preds[7], preds[8], preds[9], preds[10], preds[11], preds[12], preds[13], preds[14], preds[15], preds[16], preds[17], pref[0], pref[1], pref[2], pref[3], pref[4], pref[5], pref[6], pref[7], pref[8], pref[9], pref[10], pref[11], pref[12], pref[13], pref[14], pref[15], pref[16], pref[17], total_NPL_percentage_pass, total_NPL_val_pass, total_credit, ijp_tarif, ijp_budget, loss_limit_budget
